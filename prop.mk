bcf0832a27beb1723631aa87be2fdb38c1f73441
mahajant99 <mahajant99@gmail.com>
2019-11-10 12:08:10 +0530

    ysl: Import Heap & Dalvik Configs

--- prop.mk
+++ prop.mk
@@ -84,6 +84,15 @@ persist.vendor.cne.feature=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.coresight.config=stm-events
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m
+dalvik.vm.heapgrowthlimit=256m
+dalvik.vm.heapsize=512m
+dalvik.vm.heaptargetutilization=0.75
+dalvik.vm.heapminfree=4m
+dalvik.vm.heapmaxfree=8m
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
