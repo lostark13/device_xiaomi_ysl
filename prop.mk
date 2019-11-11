fd6bf607e795ee057e0c480a164ef4da0c49ac55
mahajant99 <mahajant99@gmail.com>
2019-11-11 20:22:14 +0530

    ysl: prop: Set default color mode to 0 * colder colors and no yellow display
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -113,7 +113,7 @@ ro.sf.lcd_density=320 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
+vendor.display.enable_default_color_mode=0 \
 vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
