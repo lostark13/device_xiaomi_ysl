421af0a80c352ff6cb277541cbd4bcf30a44d212
Roker2 <dimaminko2000@gmail.com>
2019-11-24 20:39:40 +0000

    ysl: Fix lags on some Google wallpapers
    Change-Id: I6120ff7de20f474786c3a4bca208159fe812e9ee

--- prop.mk
+++ prop.mk
@@ -101,7 +101,7 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
+debug.sf.latch_unsignaled=0 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
@@ -232,7 +232,7 @@ net.tcp.2g_init_rwnd=10
 
 # UI
 PRODUCT_PROPERTY_OVERRIDES += \
-sys.use_fifo_ui=1
+sys.use_fifo_ui=0
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
