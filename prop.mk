c97b7eef27602c497a5026d3c4e9143091c5d89a
Vaisakh Murali <vaisakhmurali@gmail.com>
2019-12-22 16:26:28 +0530

    ysl: Update dalvik props from stock
    * From ysl-user-9-PKQ1.181203.001-V10.3.3.0.PEFMIXM-release-keys
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- prop.mk
+++ prop.mk
@@ -90,11 +90,11 @@ persist.debug.coresight.config=stm-events
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m
-dalvik.vm.heapgrowthlimit=256m
-dalvik.vm.heapsize=512m
-dalvik.vm.heaptargetutilization=0.75
-dalvik.vm.heapminfree=4m
+dalvik.vm.heapstartsize=14m \
+dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.heapsize=256m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=6m \
 dalvik.vm.heapmaxfree=8m
 
 # Display
