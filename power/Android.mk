696becea0173b2168be74d2fbaffe96aefb70c55
Lostark13 <tushar9915@gmail.com>
2020-02-02 10:12:20 +0530

    ysl: Update Qcom Powerhal

--- power/Android.mk
+++ power/Android.mk
@@ -12,12 +12,11 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
+LOCAL_SRC_FILES := power-8953.c power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
 LOCAL_C_INCLUDES := external/libxml2/include \
-                     external/icu/icu4c/source/common
+                    external/icu/icu4c/source/common
 
-LOCAL_SRC_FILES += power-8953.c
 
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
@@ -28,3 +27,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
