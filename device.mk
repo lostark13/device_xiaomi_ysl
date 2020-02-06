aaaedb20bafb5c01d58bf5a7fc784dbc541e397f
Jiyong Park <jiyong@google.com>
2020-02-06 19:02:28 +0900

    ysl: Move VNDK APEX to vendor partition
    ... for aosp_* targets so that we don't need to update vendor.img everytime
    there is a change in the VNDK libraries
    
    Bug: 140136207
    Test: m
    Change-Id: Ic7ffaa9503902884f219400cf93516a6fbf8b210
    (cherry picked from commit 1d97f7055550d375aa7f371e148c3ec9aff0bd29)

--- device.mk
+++ device.mk
@@ -403,7 +403,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    vndk_package
+    vndk_package \
+    com.android.vndk.current.on_vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
