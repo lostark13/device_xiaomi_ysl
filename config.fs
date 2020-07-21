63cc2ab3eb0690fb7e1f19940befef91fc6eba01
Lostark13 <tushar9915@gmail.com>
2020-07-21 18:59:39 +0530

    ysl: Update config.fs

--- config.fs
+++ config.fs
@@ -31,12 +31,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_RADIO
@@ -61,23 +55,11 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
-
-[vendor/bin/xtwifi-client]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/sensors.qti]
 mode: 0755
