9e19b5fd34fe2ff0aeda2e5fe32c4bfc9310cde9
xyyx <xyyx@mail.ru>
2020-07-28 13:27:51 +0530

    ysl: Add basic USB HAL that reports no status change
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.ysl
 
 # Vibrator
 PRODUCT_PACKAGES += \
