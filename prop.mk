c7070f634f3692292c5a8cf44eb93b8ea4b9af9f
Lostark13 <tushar9915@gmail.com>
2020-08-15 15:12:26 +0530

    ysl: Use Xiaomi Camera Hal
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- prop.mk
+++ prop.mk
@@ -78,6 +78,7 @@ persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
+ro.camera.hardware=xiaomi.msm8953 \
 persist.camera.HAL3.enabled=1 \
 vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.android.GoogleCamera \
