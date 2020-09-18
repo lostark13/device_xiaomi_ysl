9b795ce943dc6357dd0917f94ba694d47ff59609
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    ysl: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-11-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # We modify several neverallows, so let the build proceed
