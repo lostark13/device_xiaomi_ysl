14bff0e64635617deaaa2c54a29f6ad3f7c9af56
Northn <57762732+Northn@users.noreply.github.com>
2020-12-20 13:19:32 +0400

    ysl: Initial PixysOS setup

--- octavi_ysl.mk
+++ octavi_ysl.mk
@@ -21,24 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
-# Inherit some common Octavi stuff.
-
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 #Gapps
-$(call inherit-product-if-exists, vendor/google/gms/config.mk)
-$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
+#$(call inherit-product-if-exists, vendor/google/gms/config.mk)
+#$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-OCTAVI_BUILD_TYPE := Official
-OCTAVI_DEVICE_MAINTAINER := Tushar Bharti
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
-PRODUCT_NAME := octavi_ysl
+PRODUCT_NAME := pixys_ysl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
 PRODUCT_MANUFACTURER := Xiaomi
