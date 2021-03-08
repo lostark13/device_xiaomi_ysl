0e0ad1332bf386bf67ad90f74740bf857243fb55
ItsVixano <vixanonew@gmail.com>
2021-03-08 20:18:17 +0100

    ysl: Switch to oss camera hal
    Change-Id: If62af39d99b8316a3ce551cd4d9fbeae63264716

--- device.mk
+++ device.mk
@@ -134,11 +134,16 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     android.hardware.camera.provider@2.4 \
-	android.hardware.camera.provider@2.5 \
-	android.hardware.camera.device@3.4 \
-	android.hardware.camera.device@3.2 \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.2 \
     android.frameworks.displayservice@1.0 \
+    camera.msm8953 \
+    libdng_sdk.vendor \
+    libmm-qcamera \
+    libstdc++.vendor \
     Snap
 
 # Charger
