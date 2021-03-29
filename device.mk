3ee34addf691748c802a89a3a3d717a9da5c24b8
ItsVixano <vixanonew@gmail.com>
2021-03-29 09:58:45 +0200

    ysl: Drop seccomp_policys
    * already included on device/qcom/common
    
    Change-Id: Ibcffad1162d79ea4144f01c5e171297596ca688f

--- device.mk
+++ device.mk
@@ -365,11 +365,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext \
     ims-ext-common_system
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
